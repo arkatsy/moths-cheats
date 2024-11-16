ede386ca360193f4969a1fb7b4c944464e5b6d6d
arkatsy <arkatsy@gmail.com>
2024-11-16 04:04:55 +0200

    add chakra components & basic UI for editing the save

--- src/renderer/src/components/save-card.tsx
+++ src/renderer/src/components/save-card.tsx
@@ -13,46 +13,56 @@ import iconBlizzard from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info
 import iconLeaves from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_leaves.png'
 import iconPetals from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_petals.png'
 import iconRain from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_rain.png'
-import iconRainy from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_rainy.png'
+// import iconRainy from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_rainy.png'
 import iconSnow from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_snow.png'
 import iconStorm from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_storm.png'
 import iconSunny from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_sunny.png'
-import iconThunderstorm from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_thunderstorm.png'
+// import iconThunderstorm from '@renderer/assets/sprites/weather-icons/spr_ui_hud_info_backplate_weather_icon_thunderstorm.png'
 
 const weatherIcons = [
-  { // 0: Spring
-    'calm': iconSunny,
-    'inclement': iconRain,
-    'heavy_inclement': iconStorm,
-    'special': iconPetals
+  {
+    // 0: Spring
+    calm: iconSunny,
+    inclement: iconRain,
+    heavy_inclement: iconStorm,
+    special: iconPetals
   },
-  { // 1: Summer
-    'calm': iconSunny,
-    'inclement': iconRain,
-    'heavy_inclement': iconStorm,
-    'special': "summer_special"
+  {
+    // 1: Summer
+    calm: iconSunny,
+    inclement: iconRain,
+    heavy_inclement: iconStorm,
+    special: 'summer_special'
   },
-  { // 2: Fall
-    'calm': iconSunny,
-    'inclement': iconRain,
-    'heavy_inclement': iconStorm,
-    'special': iconLeaves
+  {
+    // 2: Fall
+    calm: iconSunny,
+    inclement: iconRain,
+    heavy_inclement: iconStorm,
+    special: iconLeaves
   },
-  { // 3: Winter
-    'calm': iconSunny,
-    'inclement': iconSnow,
-    'heavy_inclement': iconBlizzard,
-    'special': "winter_special"
+  {
+    // 3: Winter
+    calm: iconSunny,
+    inclement: iconSnow,
+    heavy_inclement: iconBlizzard,
+    special: 'winter_special'
   }
 ]
 
 type Save = SortedLoadingSaves[number]
 
-function SaveCard({ save }: { save: Save }) {
+function SaveCard({ save, onClick }: { save: Save; onClick?: (saveId: string) => void }) {
+  const { header, autosave, id } = save
+
   const data = {
-    Gold: Intl.NumberFormat().format(save.header.stats.gold),
-    Essence: Intl.NumberFormat().format(save.header.stats.essence),
-    Renown: Intl.NumberFormat().format(save.header.stats.renown)
+    Gold: Intl.NumberFormat().format(header.stats.gold),
+    Essence: Intl.NumberFormat().format(header.stats.essence),
+    Renown: Intl.NumberFormat().format(header.stats.renown)
+  }
+
+  function handleClick() {
+    onClick && onClick(id)
   }
 
   return (
@@ -63,27 +73,34 @@ function SaveCard({ save }: { save: Save }) {
       w="full"
       mx="auto"
       cursor="pointer"
+      onClick={handleClick}
     >
       <Card.Header display="flex" flexDir="row" justifyContent="space-between" pos="relative">
         <Flex flexDir="column">
-          <Text textStyle="xl">
-            {save.header.name} | {displayCalendarTime(save.header.calendar_time)} -&nbsp;
-            {displayClockTime(save.header.clock_time)} &nbsp;
-            <Box as="span">{save.autosave && '(autosave)'}</Box>
+          <Text textStyle="xl" display="flex" gap={2} alignItems="center">
+            {header.name} | {displayCalendarTime(header.calendar_time)}
+            <Image
+              display="inline"
+              h="25px"
+              w="25px"
+              src={
+                weatherIcons[displayWeather(header.calendar_time, header.weather.forecast)[0]][
+                  displayWeather(header.calendar_time, header.weather.forecast)[1]
+                ]
+              }
+            />
+            -&nbsp;
+            {displayClockTime(header.clock_time)} &nbsp;
+            <Box as="span">{autosave && '(autosave)'}</Box>
           </Text>
-          <Image
-            h="25px"
-            w="25px"
-            src={weatherIcons[displayWeather(save.header.calendar_time, save.header.weather.forecast)[0]][displayWeather(save.header.calendar_time, save.header.weather.forecast)[1]]}
-          />
         </Flex>
 
         <Box pos="relative">
           <Text textStyle="xl" textAlign="end" ml={2}>
-            {save.header.farm_name}
+            {header.farm_name}
           </Text>
           <Text pos="absolute" textStyle="xl" opacity={0.9} right="0">
-            {displayPlaytime(save.header.playtime)}
+            {displayPlaytime(header.playtime)}
           </Text>
         </Box>
       </Card.Header>
@@ -100,7 +117,7 @@ function SaveCard({ save }: { save: Save }) {
         </Flex>
         <Box pos="absolute" right={6} bottom={7}>
           <Text fontSize="sm" opacity="0.8">
-            {save.id}
+            {id}
           </Text>
         </Box>
       </Card.Body>
